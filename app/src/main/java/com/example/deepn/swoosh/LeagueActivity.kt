5c0d835bdbd3d7d6dcb9458817098295083e63ab
sakhayadeep <sakhayadeepnath@gmail.com>
2018-04-29 23:15:30 +0530

    Added league selecter activity

---
+++ app/src/main/java/com/example/deepn/swoosh/LeagueActivity.kt
@@ -0,0 +1,12 @@
+package com.example.deepn.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
