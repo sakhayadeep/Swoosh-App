ac69cbc742f8f0ef9f19d69c04c2cd32b0719402
sakhayadeep <sakhayadeepnath@gmail.com>
2018-04-30 00:21:47 +0530

    intent added to move to League selecter

--- app/src/main/java/com/example/deepn/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/deepn/swoosh/WelcomeActivity.kt
@@ -1,12 +1,18 @@
 package com.example.deepn.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        getstartedbtn.setOnClickListener {
+            val leagueIntent = Intent(this,LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
